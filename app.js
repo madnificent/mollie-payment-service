c66e08c5f3208562373b91befa8e2917ec999f1d
Aad Versteden <madnificent@gmail.com>
2019-07-11 16:49:04 +0200

    Write status based on webhook
    When we receive the status from the Mollie API, we now set it as a
    string on the basket.  This should probably become a smarter semantic
    model in the future, yet it should also suffice for now.
    
    The status can be rendered in the frontend to indicate what happened
    with the basket.  The construction with a trivial string can later be
    extended so the backend talks in URIs whilst the frontend keeps its
    current string properties.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // see https://github.com/mu-semtech/mu-javascript-template for more info
 
-import { app, query, errorHandler } from 'mu';
+import { app, query, update, errorHandler, sparqlEscapeUri, sparqlEscapeString } from 'mu';
 import assert from 'assert';
 import mollieConstructor from '@mollie/api-client';
+import bodyParser from 'body-parser';
 
 const MOLLIE_API_KEY = "YOUR MOLLIE API KEY";
 const MOLLIE_REDIRECT_URL = "http://link-to-redirect-app-site/";
@@ -36,6 +37,7 @@ app.post('/payments', asyncMiddleware( async function( req, res, next ) {
       currency: 'EUR'
     },
     description, redirectUrl,
+    metadata: { sessionId: req.get('mu-session-id') },
     webhookUrl:  MOLLIE_BASE_WEBHOOK_URL,
     // method: ["applepay","bancontact","banktransfer","belfius","creditcard","directdebit","ideal","inghomepay","kbc","paypal"]
   });
@@ -53,9 +55,43 @@ app.post('/payments', asyncMiddleware( async function( req, res, next ) {
     } ) );
 } ) );
 
-app.post('webhook', function( req, res ) {
+app.post('/webhook', async function( req, res ) {
   console.log("Received webhook trigger");
-  console.log(res.body);
+  console.log(JSON.stringify(req.headers));
+  console.log(req.body);
+
+  const paymentId = req.body.id;
+
+  try {
+    const client = makeMollieClient();
+    const paymentInfo = await client.payments.get(paymentId);
+
+    const sessionId = paymentInfo.metadata.sessionId;
+    const paymentStatus = paymentInfo.status;
+
+    // TODO: send with updated mu-session-id header
+    const response = await update(`
+      PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
+      PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+
+      WITH <http://mu.semte.ch/application>
+      DELETE {
+        ?basket veeakker:basketPaymentStatus ?paymentStatus.
+      }
+      INSERT {
+        ?basket veeakker:basketPaymentStatus ${sparqlEscapeString(paymentStatus)}.
+      }
+      WHERE {
+        ${sparqlEscapeUri(sessionId)} veeakker:hasBasket ?basket.
+        OPTIONAL { ?basket veeakker:basketPaymentStatus ?paymentStatus. }
+      }
+    `);
+
+    // TODO: cope with erroneous responses from Mollie
+  } catch (err) {
+    console.log(err);
+  }
+  res.send("Success");
 } );
 
 
